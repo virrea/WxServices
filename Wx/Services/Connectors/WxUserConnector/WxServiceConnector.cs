83c8290a75e285e8863d149d7713ee2fb1d1e8ea
unknown <Olivier@.(none)>
2011-09-04 22:46:43 +0200

    Adding some methods to query the user database and update a user. Fixing typos.

--- Wx/Services/Connectors/WxUserConnector/WxServiceConnector.cs
+++ Wx/Services/Connectors/WxUserConnector/WxServiceConnector.cs
@@ -28,7 +28,7 @@ namespace Wx.Connector
     public class WxUserServiceConnector : ServiceConnector
     {
 
-        private IWxUserService m_WxUserService;
+        private IUserAccountService m_UserAccountService;
         private string m_ConfigName = "WxUserService";
         // Our connector
         // We load this first. See the ini [Startup] ServiceConnectors
@@ -59,9 +59,9 @@ namespace Wx.Connector
                 throw new Exception("No LocalServiceModule in config file");
 
             Object[] args = new Object[] { config };
-            m_WxUserService = ServerUtils.LoadPlugin<IWxUserService>(WxUserService, args);
+            m_UserAccountService = ServerUtils.LoadPlugin<IUserAccountService>(WxUserService, args);
 
-            server.AddStreamHandler(new WxUsersHandler(m_WxUserService));
+            server.AddStreamHandler(new WxUsersHandler(m_UserAccountService));
         }
     }
 }
